4c6b2f452cfc6416dc7267014cd03bc839d93375
Mahlatse Masemola <ST10495426@rcconnect.edu.za>
2026-06-18 04:21:51 +0200

    Add MySQL database setup and DatabaseHelper class

---
+++ CybersecurityAwarenessChatBotGUI/DatabaseHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace CybersecurityAwarenessChatBotGUI
+{
+    public static class DatabaseHelper
+    {
+        
+        private static string connectionString = "Server=localhost;Port=3306;Database=cybersecurity_bot;Uid=root;Pwd=My049g4amm8ng@;";
+
+        public static MySqlConnection GetConnection()
+        {
+            try
+            {
+                return new MySqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Database connection failed: " + ex.Message);
+            }
+        }
+
+        public static void TestConnection()
+        {
+            using (var conn = GetConnection())
+            {
+                conn.Open();
+                Console.WriteLine("Database connection successful!");
+            }
+        }
+    }
+}
